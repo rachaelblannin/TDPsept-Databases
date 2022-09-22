9df85ebb851b3daca09ee486ea15b19e74714d14
rachaelblannin <rachblannin@gmail.com>
2022-09-22 16:21:02 +0100

    initial commit

--- Answers.sql
+++ Answers.sql
@@ -2,4 +2,19 @@ USE world;
 SHOW TABLES;
 SELECT * FROM city;
 SELECT * FROM country;
-SELECT * COUNT FROM city WHERE countrycode = USA; 
+SELECT * count(*) FROM city WHERE countrycode = "USA";
+SELECT count(*) FROM city WHERE countrycode = "USA";
+#274
+
+SHOW TABLES;
+SELECT * from country;
+
+SELECT population FROM country WHERE Name="Argentina";
+#37032000
+
+SELECT * FROM country ORDER BY LifeExpectancy DESC;
+#Highest Life expectancy is Andorra
+
+
+
+
