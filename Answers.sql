f2022deb8f2d1f0896bef8141b55d27e71e5709b
rachaelblannin <rachblannin@gmail.com>
2022-09-23 10:33:46 +0100

    initial commit

--- Answers.sql
+++ Answers.sql
@@ -15,6 +15,21 @@ SELECT population FROM country WHERE Name="Argentina";
 SELECT * FROM country ORDER BY LifeExpectancy DESC;
 #Highest Life expectancy is Andorra
 
+USE world;
+SELECT * FROM country; 
+SHOW TABLES;
+SELECT * FROM city;
+SELECT * FROM country; 
+
+SELECT * FROM city JOIN country ON city.ID=country.Capital WHERE country.Name="Spain";
+#Madrid
+
+SHOW TABLES;
+SELECT * FROM countrylanguage;
+SELECT * FROM country;
+SELECT countrylanguage.Language FROM countrylanguage JOIN country ON countrylanguage.CountryCode=country.Code WHERE country.Region="Southeast Asia";
+
+
 
 
 
